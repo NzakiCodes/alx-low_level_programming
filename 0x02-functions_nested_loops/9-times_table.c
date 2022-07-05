80b5baad473e0d4f86c3ac19a2cea2dfcd89a9d2
Ekikere-Abasi Ekere <ekisnzaki@gmail.com>
2022-07-05 09:32:42 -0700

    dixed 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,39 @@
-#include "main.h"
+#include "holberton.h"
 /**
-  * times_table - The entry point of the function
-  * @void: The function takes no parameters
-  *
-  * Description: This is a function that prints the 9 times table, starting
-  * with 0.
-  */
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
+ */
 void times_table(void)
 {
-	int firstnum, secondnum, multiplication_result;
+	int rone, cone, d;
 
-	firstnum = 0;
-	while (firstnum < 10)
+	for (rone = 0; rone <= 9; rone++)
 	{
-		secondnum = 0;
-		while (secondnum < 10)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
 		{
-			multiplication_result = firstnum * secondnum;
-			if (secondnum != 9)
+			d = (rone * cone);
+			if ((d / 10) > 0)
 			{
-				if ((multiplication_result / 10) == 0)
-				{
-					_putchar(multiplication_result + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar((multiplication_result / 10) + '0');
-					_putchar((multiplication_result % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar((d / 10) + '0');
 			}
 			else
 			{
-				if ((multiplication_result / 10) == 0)
-					_putchar(multiplication_result + '0');
-				else
-				{
-					_putchar((multiplication_result / 10) + '0');
-					_putchar((multiplication_result % 10) + '0');
-				}
+				_putchar(' ');
 			}
-			secondnum++;
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		firstnum++;
 		_putchar('\n');
 	}
-}
+
+}}
