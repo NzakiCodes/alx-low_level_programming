9fe3e1d13b7c0b903e415a4d0758cdd55ce77c96
Ekikere-Abasi Ekere <ekisnzaki@gmail.com>
2022-07-05 09:39:18 -0700

    Added 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * print_last_digit - The entry point of the function
-  * @r: The number to be checked
+  * @n: The number to be checked
   *
   * Description: This is a function that prints the last digit of a number.
   * Return: last_digt
